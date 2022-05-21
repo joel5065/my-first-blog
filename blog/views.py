a73616ccd0eb592b368e47b2d47bd3d2497b2b83
joel5065 <kouakoujoel7@gmail.com>
2022-05-21 09:48:40 +0000

    changed m

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
+from os import rename
 from django import template
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from django.template import loader
 from .models import Post
 
@@ -12,3 +14,13 @@ def post_list(request):
         'posts': posts,
         }
     return HttpResponse(template.render(context, request))
+
+def post_detail(request, pk):
+    post_detail = Post.objects.get(pk=pk)
+    template = loader.get_template('blog/post_detail.html')
+    context = {
+        'post_detail': post_detail,
+    }
+    return HttpResponse(template.render(context, request))
+
+    
